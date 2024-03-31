f2221384259a99864119ef41b19cdc0221769376
涑尼 <1774452411@qq.com>
2024-03-31 22:14:10 +0800

    添加查询接口，根据id，分页

--- msg-push-web/src/main/java/com/jack/msgpush/web/controller/MessageTemplateController.java
+++ msg-push-web/src/main/java/com/jack/msgpush/web/controller/MessageTemplateController.java
@@ -2,14 +2,15 @@ package com.jack.msgpush.web.controller;
 
 import com.jack.msgpush.support.domin.MessageTemplate;
 import com.jack.msgpush.web.service.MessageTemplateService;
+import com.jack.msgpush.web.vo.MessageTemplateParam;
+import com.jack.msgpush.web.vo.MessageTemplateVo;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 消息模板管理controller
@@ -29,5 +30,16 @@ public class MessageTemplateController {
         return messageTemplateService.saveOrUpdate(messageTemplate);
     }
 
+    @GetMapping("/query/{id}")
+    @Operation(summary = "根据id查询")
+    public MessageTemplate queryById(@PathVariable("id") Long id) {
+        return messageTemplateService.queryById(id);
+    }
 
+    @GetMapping("/query/list")
+    @Operation(summary = "列表查询")
+    public MessageTemplateVo queryList(@Validated MessageTemplateParam messageTemplateParam) {
+        Page<MessageTemplate> messageTemplates = messageTemplateService.queryList(messageTemplateParam);
+        return MessageTemplateVo.builder().rows(messageTemplates.toList()).count(messageTemplates.getTotalElements()).build();
+    }
 }
--- msg-push-web/src/main/java/com/jack/msgpush/web/service/impl/MessageTemplateServiceImpl.java
+++ msg-push-web/src/main/java/com/jack/msgpush/web/service/impl/MessageTemplateServiceImpl.java
@@ -5,7 +5,10 @@ import com.jack.msgpush.common.constant.CommonConstant;
 import com.jack.msgpush.support.dao.MessageTemplateDao;
 import com.jack.msgpush.support.domin.MessageTemplate;
 import com.jack.msgpush.web.service.MessageTemplateService;
+import com.jack.msgpush.web.vo.MessageTemplateParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -23,6 +26,17 @@ public class MessageTemplateServiceImpl implements MessageTemplateService {
 
         messageTemplate.setUpdated(Math.toIntExact(DateUtil.currentSeconds()));
         return messageTemplateDao.save(messageTemplate);
+    }
+
+    @Override
+    public MessageTemplate queryById(Long id) {
+        return messageTemplateDao.findById(id).orElse(null);
+    }
+
+    @Override
+    public Page<MessageTemplate> queryList(MessageTemplateParam param) {
+        PageRequest pageRequest = PageRequest.of(param.getPage() - 1, param.getPageSize());
+        return messageTemplateDao.findAll(pageRequest);
     }
 
 }
